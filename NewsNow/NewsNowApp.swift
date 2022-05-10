84760d349796ad5e35f71e1b406bb218b30085bb
komplektan <54449303+komplektan@users.noreply.github.com>
2022-05-10 16:10:55 +0500

    Initial Commit

---
+++ NewsNow/NewsNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsNowApp.swift
+//  NewsNow
+//
+//  Created by Вячеслав Квашнин on 10.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
